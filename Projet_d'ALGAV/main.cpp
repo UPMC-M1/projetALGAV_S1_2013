9b49f4b3d32a8cf4f4ab114f66e8063b6bde6a24
iShavgula <g.shavgula@gmail.com>
2013-11-23 18:20:00 +0100

    Added project

---
+++ Projet_d'ALGAV/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Projet_d'ALGAV
+//
+//  Created by Giorgi Shavgulidze on 23/11/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
