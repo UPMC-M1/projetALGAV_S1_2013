74b2fd53a34f882bc1c105b21bd0b8ca5174ca36
iShavgula <g.shavgula@gmail.com>
2013-11-26 10:39:58 +0100

    From Zero

--- Projet_d'ALGAV/main.cpp
+++ Projet_d'ALGAV/main.cpp
@@ -1,18 +1,84 @@
-//
-//  main.cpp
-//  Projet_d'ALGAV
-//
-//  Created by Giorgi Shavgulidze on 23/11/13.
-//
-//
-
 #include <iostream>
+#include "algorithm"
+#include <map>
+
+using namespace std;
+
+// struct definitions
+
+
+typedef struct node node_t;
+
+struct node {
+    unsigned char key;        // character corresponding to node
+    int weight;            // number of character in the text for the current time
+    int code;        // code used in algorithm to define height of the node
+    node_t *left;           // left subtree
+    node_t *right;        // right subtree
+    node_t *father;     //his father
+};
+
+// globals declarations
+
+node_t root;
+map <unsigned char, int> characterCode;
+
+
+// function declarations
+
+string getText();
+void processText(string);
+void init();
+
+// function definitions
+
+void init() {
+    root.key = '#';
+    root.weight = 0;
+    root.left = nullptr;
+    root.right = nullptr;
+    root.father = nullptr;
+    root.code = 255;
+    
+    characterCode['#'] = 0;
+}
+
+//function initialize the node as a special node
+
+
+
+
+//this function initialize the node like a new Leaf with a key character
+
+
+
+void processText(string text) {
+    void init();
+    for (int i = 0; i < text.size(); i++) {
+        
+    }
+    
+}
+
+
+bool isSpecialSymbol(node_t *node) {
+    if (node->weight == 0)
+        return true;
+    return false;
+}
+
+
+
+
+
+
 
 int main(int argc, const char * argv[])
 {
-	// coucou
-    // coucou vol2
-    std::cout << "Hello, World!\n";
+    string text;
+    
+    text = getText();
+    processText(text);
+    
     return 0;
 }
-
